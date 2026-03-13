fe6dc70f1c472ab96d6a92f45ad1a228c57585f5
damesck233 <damesck@qq.com>
2026-03-13 16:38:19 +0800

    Load background image from external CDN URL

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -29,7 +29,7 @@ import DynamicIslandNav from './components/DynamicIslandNav'
 import RestrictedAccess from './components/RestrictedAccess'
 import ScrollToTop from './components/ScrollToTop'
 import { preloadResourcesWithMinTime } from './utils/preloader'
-const bgImage = '/img/Background_image.webp'
+const bgImage = 'https://img.klpz.net/file/tc/2026/03/13/69b3cbfa2648f_1773390842.webp'
 const bgPlaceholder = 'data:image/webp;base64,UklGRnYAAABXRUJQVlA4IGoAAADwAwCdASoUAA0APzmEuVOvKKWisAgB4CcJZgCdEf/gPjzNvF5J8s1wAPfa9nR9ymMZZjBVj6isAgWQSgCrEdElEDGRKAq8rb0S2RWQUQoT7MFnf4HPdmIsTiW+I5Tryd7G1yX8IYDqAAAA'
 
 // 将背景图片组件分离，减少重渲染
